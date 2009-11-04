944ca1aa72655b6d8ab306ec344162ac13f0b002
Christoph Helma <helma@in-silico.de>
2009-11-04 11:14:15 +0100

    bbrc features added to resources for cleanup hook

--- features/step_definitions/fminer_step.rb
+++ features/step_definitions/fminer_step.rb
@@ -4,4 +4,5 @@ When /^I apply fminer$/ do
 	call = "curl -X PUT -F 'file=@#{@filename};type=text/csv' -F compound_format=#{@params['compound_format']} #{@training_uri + '/import'}"
 	`#{call}`
 	@uri = RestClient.post File.join(@@config[:services]["opentox-algorithm"], "fminer"), :dataset_uri => @training_uri
+	@resources << @uri
 end
